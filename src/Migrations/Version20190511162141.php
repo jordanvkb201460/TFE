3845bb4e58460ca1c68c6d865e0ae6e1417094f0
jordan <jordan.vkb@gmail.com>
2019-05-11 16:23:34 +0000

    rating participant

--- src/Migrations/Version20190511162141.php
+++ src/Migrations/Version20190511162141.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190511151708 extends AbstractMigration
+final class Version20190511162141 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,8 @@ final class Version20190511151708 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE participant_experience (participant_id INT NOT NULL, experience_id INT NOT NULL, INDEX IDX_3950271D9D1C3019 (participant_id), INDEX IDX_3950271D46E90E27 (experience_id), PRIMARY KEY(participant_id, experience_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE participant_experience ADD CONSTRAINT FK_3950271D9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE participant_experience ADD CONSTRAINT FK_3950271D46E90E27 FOREIGN KEY (experience_id) REFERENCES experience (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE participation_request ADD status INT NOT NULL');
+        $this->addSql('ALTER TABLE participant_experience DROP status');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +31,7 @@ final class Version20190511151708 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE participant_experience');
+        $this->addSql('ALTER TABLE participant_experience ADD status INT NOT NULL');
         $this->addSql('ALTER TABLE participation_request DROP status');
     }
 }
